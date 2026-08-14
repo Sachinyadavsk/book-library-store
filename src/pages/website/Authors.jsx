8bfefc0287d302ea9a22e61f512fb2f75eaa6688
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-08-14 18:11:19 +0530

    contact and authers page craete

--- src/pages/website/Authors.jsx
+++ src/pages/website/Authors.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import authresimag1 from '../../assets/authers1.jpg';
+import authresimag2 from '../../assets/authers2.jpg';
+import authresimag3 from '../../assets/authers3.jpg';
 
 const Authors = () => {
   return (
@@ -6,6 +9,67 @@ const Authors = () => {
       <div className="max-w-3xl mx-auto text-center">
         <h3 className='text-3xl text-gray-600 font-semibold'>Authors Publications</h3>
         <p className='text-lg text-gray-500 my-2'>Authors list and wih relation the books name</p>
+      </div>
+
+      {/* authers list */}
+      <div className="max-w-4xl mx-auto">
+        <div className="shadow rounded-3xl py-2 px-5 mt-4">
+          <div className="flex gap-15">
+            <div className="">
+              <img
+                src={authresimag1}
+                alt="Profile"
+                className="w-32 h-32 sm:w-32 sm:h-32 lg:rounded-full object-cover border-4 border-white shadow-lg"
+              />
+            </div>
+            <div className="">
+              <h1 className="mt-5 text-2xl font-bold text-gray-800">
+                Hello
+              </h1>
+              <p className="mt-2 text-sm sm:text-base text-gray-500">
+                Welcome to our Book Library Store.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded-3xl py-2 px-5 mt-4">
+          <div className="flex gap-15">
+            <div className="">
+              <img
+                src={authresimag2}
+                alt="Profile"
+                className="w-32 h-32 sm:w-32 sm:h-32 lg:rounded-full object-cover border-4 border-white shadow-lg"
+              />
+            </div>
+            <div className="">
+              <h1 className="mt-5 text-2xl font-bold text-gray-800">
+                Hello
+              </h1>
+              <p className="mt-2 text-sm sm:text-base text-gray-500">
+                Welcome to our Book Library Store.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded-3xl py-2 px-5 mt-4">
+          <div className="flex gap-15">
+            <div className="">
+              <img
+                src={authresimag3}
+                alt="Profile"
+                className="w-32 h-32 sm:w-32 sm:h-32 lg:rounded-full object-cover border-4 border-white shadow-lg"
+              />
+            </div>
+            <div className="">
+              <h1 className="mt-5 text-2xl font-bold text-gray-800">
+                Hello
+              </h1>
+              <p className="mt-2 text-sm sm:text-base text-gray-500">
+                Welcome to our Book Library Store.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/pages/website/Contact.jsx
+++ src/pages/website/Contact.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocation, faMailForward, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="max-w-full mx-auto py-10 px-4">
+      <div className="max-w-3xl mx-auto py-5 px-5 text-center">
+        <h6 className='text-4xl text-gray-500 font-bold'>Contact us</h6>
+        <p className='text-xl text-gray-500 mt-3'>Information with query get</p>
+      </div>
+      <div className="max-w-6xl mx-auto py-5 px-5">
+        <div className="grid lg:grid-cols-2 gap-10">
+          <div className="">
+            <div className="shadow-2xl py-5 px-5">
+              <form action="" method="post">
+                <div className="">
+                  <label htmlFor="">Your name</label>
+                  <input type="text" placeholder='Enter your name' className='w-full p-3 border' />
+                </div>
+                <div className="mt-2">
+                  <label htmlFor="">Email Address</label>
+                  <input type="Email" placeholder='Enter email address' className='w-full p-3 border' />
+                </div>
+                <div className="mt-2">
+                  <label htmlFor="">Message</label>
+                  <textarea name="message" id="" cols="30" rows="8" placeholder='Enter your message Or typing' className='w-full p-3 border'></textarea>
+                </div>
+                <div className="">
+                  <button type="submit" className='bg-blue-600 text-gray-200 border p-3 mt-2'>Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="">
+            <div className="flex gap-10">
+              <div className=""> <FontAwesomeIcon icon={faMailForward} className='text-blue-500 rounded-2xl object-cover border-2 p-4' /></div>
+              <div className="">
+                <h6>Email</h6>
+                <p>book@gmail.com</p>
+              </div>
+              <div className=""> <FontAwesomeIcon icon={faPhone} className='text-blue-500 rounded-2xl object-cover border-2 p-4' /></div>
+              <div className="">
+                <h6>Phone</h6>
+                <p>411-54545-5745</p>
+              </div>
+              <div className=""><FontAwesomeIcon icon={faLocation} className='text-blue-500 rounded-2xl object-cover border-2 p-4' /></div>
+              <div className="">
+                <h6>Address</h6>
+                <p>k-52 block center noida uttar pradesh</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
